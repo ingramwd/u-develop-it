f9c450dd7471b33995bc8cbe514c0abc98960996
ingramwd <Ingramwd01@gmail.com>
2022-01-18 18:26:51 -0500

    created first query

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,14 +8,31 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connects to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your MySQL username,
+        user: 'root',
+        // MySQL password
+        password: '850031Wdi!',
+        database: 'election'
+    },
+    console.log('connected to the election database.')
+);
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
     });
 });
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 // Default response for any other request (NOT FOUND) 
-//ALWAYS PLACE AS LAST ROUTE
+//ALWAYS PLACE AS LAST ROUTE IT TAKES PRECEDENT
 app.use((req, res) => {
     res.status(404).end();
 });
